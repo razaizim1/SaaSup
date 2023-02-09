515ee26f1af9af436dc8a100b47e9ef5f694d835
Razai Zim <razai.zim1@gamil.com>
2023-02-09 23:06:41 +0600

    Responsive

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,48 @@
-$(document).ready(function () {
-  // MagnificPopUp
 
+
+// Animation
+TweenMax.staggerFrom("nav ul li", 1, {
+  opacity: 0,
+  x: -20,
+  ease: Power3.easeInOut
+}, 0.08);
+
+TweenMax.from(".logo",1, {
+  opacity: 0,
+  x: -20,
+  ease: Expo.easeInOut
+});
+
+TweenMax.from(".menu-btn a",1, {
+  opacity: 0,
+  x: -20,
+  ease: Expo.easeInOut,
+  delay: .6
+});
+
+TweenMax.from(".banner-content ul li .video-btn",1,{
+  opacity: 0,
+  delay: 1,
+  x: -200,
+  ease: Expo.easeInOut
+})
+TweenMax.from(".banner-content ul li .btn-2",1,{
+  opacity: 0,
+  delay: 2,
+  x: -200,
+  ease: Elastic.easeOut.config(1,0.3)
+})
+
+
+$(document).ready(function () {
+
+
+  // Mobile Menu
+  $(".nav-btn").click(function(){
+    $(".menu").slideToggle(1000);
+  })
+
+  // MagnificPopUp
   $(".youtube-popup").magnificPopup({
     type: "iframe", // this is default type
   });
@@ -14,3 +56,6 @@ tl.fromTo(".box", { width: "100%" }, { width: "0", duration: 1, delay: 1 });
 
 // PURE COUNTER
 new PureCounter();
+
+
+
